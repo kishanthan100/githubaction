bc01eab105e16bf18ed0d8be351e662505db9d92
kishanthan100 <sivathamil259@gmail.com>
2025-06-23 16:58:25 +0530

    adjust the function's parameters

--- tests/test_op.py
+++ tests/test_op.py
@@ -3,5 +3,5 @@ from src.math_op import add, sub
 def test_add():
     assert add(2,3)==5
 def test_sun():
-    assert sub(6-2)==4
-    assert sub(3-5)==-2
+    assert sub(6,2)==4
+    assert sub(3,5)==-2
