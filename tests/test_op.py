d67fa161d84a0e50120c8af1dec6952c1faba967
kishanthan100 <sivathamil259@gmail.com>
2025-06-23 17:20:15 +0530

    add muliplication to the test case

--- tests/test_op.py
+++ tests/test_op.py
@@ -1,4 +1,4 @@
-from src.math_op import add, sub
+from src.math_op import add, sub, mul
 
 def test_add():
     assert add(2,3)==5
@@ -7,3 +7,5 @@ def test_sun():
     assert sub(6,2)==4
     assert sub(3,5)==-2
     assert sub(2,5)==-3
+def test_mul():
+    assert mul(5,5)==25
